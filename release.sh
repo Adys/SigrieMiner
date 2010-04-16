f340299817224b5f7b0eac6dc03af7b04b2dfce1
Jerome Leclanche <jerome@leclan.ch>
2010-04-16 22:28:27 +0300

    Use --output as -o isn't understood by gentoo...

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $(git show-ref --head HEAD | awk '{print $1}') > VERSION
-git archive HEAD -o +MMOC_Recorder.tar
+git archive HEAD --output=+MMOC_Recorder.tar
 tar --append --file=+MMOC_Recorder.tar VERSION
 bzip2 +MMOC_Recorder.tar
 mv +MMOC_Recorder.tar.bz2 VERSION /var/www/sigrie/sigrie/sigrie/static/addon/
