40d51c31be68999879c0d82a699d06210d1bd7a0
Jerome Leclanche <jerome@leclan.ch>
2010-04-24 15:04:01 +0300

    Change release script to plain files instead of tar

--- release.sh
+++ release.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 git show-ref -s --heads master > VERSION
-git archive HEAD --output=+MMOC_Recorder.tar
-tar --append --file=+MMOC_Recorder.tar VERSION
-bzip2 +MMOC_Recorder.tar
-cp +MMOC_Recorder.tar.bz2 VERSION /var/www/sigrie/sigrie/sigrie/static/addon/
-rm +MMOC_Recorder.tar.bz2
+cp MMOC_Recorder.toc *.lua VERSION /var/www/sigrie/sigrie/sigrie/static/addon/
