dab6c718f160bbb62d7739d18590ff7eb4de16be
Jerome Leclanche <jerome@leclan.ch>
2010-04-24 15:55:52 +0300

    Fix release.sh typo

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git show-ref -s --heads master > VERSION
-cp MMOC_Recorder.toc *.lua VERSION /var/www/sigrie/sigrie/sigrie/static/addon/
+cp +MMOC_Recorder.toc *.lua VERSION /var/www/sigrie/sigrie/sigrie/static/addon/
